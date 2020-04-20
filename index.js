a42c67fa8d08d0ac5e5d7e8993e95d0a15570f71
Nawanit <sharma.nawanit123@gmail.com>
2020-04-20 20:32:29 +0530

    Successful game run 1

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 3;
+const cells = 6;
 const width = 600;
 const height = 600;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -23,10 +24,10 @@ Runner.run(Runner.create(), engine);
 
 //Walls
 const walls = [
-  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 2, height, { isStatic: true }),
 ];
 World.add(world, walls);
 
@@ -116,8 +117,9 @@ horizontals.forEach((row, rowIndex) => {
       columnIndex * unitLength + unitLength / 2,
       rowIndex * unitLength + unitLength,
       unitLength,
-      10,
+      5,
       {
+        label: "wall",
         isStatic: true,
       }
     );
@@ -131,12 +133,72 @@ verticals.forEach((row, rowIndex) => {
     const wall = Bodies.rectangle(
       columnIndex * unitLength + unitLength,
       rowIndex * unitLength + unitLength / 2,
-      10,
+      5,
       unitLength,
       {
+        label: "wall",
         isStatic: true,
       }
     );
     World.add(world, wall);
+  });
+});
+
+//Goal
+const goal = Bodies.rectangle(
+  width - unitLength / 2,
+  height - unitLength / 2,
+  unitLength * 0.7,
+  unitLength * 0.7,
+  {
+    label: "goal",
+    isStatic: true,
+  }
+);
+
+World.add(world, goal);
+
+//Drawing Ball
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball",
+});
+World.add(world, ball);
+
+//Adding event listeners
+document.addEventListener("keydown", (event) => {
+  const { x, y } = ball.velocity;
+  if (event.keyCode === 87 || event.keyCode === 38) {
+    //up
+    Body.setVelocity(ball, { x, y: y - 5 });
+  }
+  if (event.keyCode === 68 || event.keyCode === 39) {
+    //right
+    Body.setVelocity(ball, { x: x + 5, y: y + 5 });
+  }
+  if (event.keyCode === 83 || event.keyCode === 40) {
+    //down
+    Body.setVelocity(ball, { x, y: y + 5 });
+  }
+  if (event.keyCode === 65 || event.keyCode === 37) {
+    //left
+    Body.setVelocity(ball, { x: x - 5, y });
+  }
+});
+
+//Win Conditions
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["goal", "ball"];
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      world.gravity.y = 1;
+      world.bodies.forEach((body) => {
+        if (body.label === "wall") {
+          Body.setStatic(body, false);
+        }
+      });
+    }
   });
 });
