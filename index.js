63177931854eb6aa150b1a99a860ceb6d2b1c492
Nawanit <sharma.nawanit123@gmail.com>
2020-04-20 22:52:01 +0530

    game in playable state & responsive to all screens

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
+const html = document.querySelector("html");
+const cellsHorizontal = 8;
+const cellsVertical = 5;
+const width = html.clientWidth;
+const height = html.clientHeight;
 
-const cells = 6;
-const width = 600;
-const height = 600;
-
-const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0;
@@ -13,7 +15,7 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: true,
+    wireframes: false,
     width,
     height,
   },
@@ -31,10 +33,6 @@ const walls = [
 ];
 World.add(world, walls);
 
-//Grid generations
-// const arr1 =Array(cells).fill(false);
-// const arr2 =Array(cells-1).fill(false);
-
 const shuffle = (arr) => {
   let counter = arr.length;
 
@@ -49,18 +47,18 @@ const shuffle = (arr) => {
   return arr;
 };
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells).fill(false));
-const verticals = Array(cells)
+  .map(() => Array(cellsHorizontal).fill(false));
+const verticals = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false));
-const horizontals = Array(cells - 1)
+  .map(() => Array(cellsHorizontal - 1).fill(false));
+const horizontals = Array(cellsVertical - 1)
   .fill(null)
-  .map(() => Array(cells).fill(false));
+  .map(() => Array(cellsHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 const stepThroughCell = (row, column) => {
   //If I have visited the cell at [row,column], then return
@@ -83,9 +81,9 @@ const stepThroughCell = (row, column) => {
     //See if that neighbor is out of bounds
     if (
       nextRow < 0 ||
-      nextRow >= cells ||
+      nextRow >= cellsVertical ||
       nextColumn < 0 ||
-      nextColumn >= cells
+      nextColumn >= cellsHorizontal
     ) {
       continue;
     }
@@ -109,14 +107,15 @@ const stepThroughCell = (row, column) => {
 };
 
 stepThroughCell(startRow, startColumn);
+
 horizontals.forEach((row, rowIndex) => {
   row.forEach((open, columnIndex) => {
     if (open) return;
 
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength / 2,
-      rowIndex * unitLength + unitLength,
-      unitLength,
+      columnIndex * unitLengthX + unitLengthX / 2,
+      rowIndex * unitLengthY + unitLengthY,
+      unitLengthX,
       5,
       {
         label: "wall",
@@ -131,10 +130,10 @@ verticals.forEach((row, rowIndex) => {
     if (open) return;
 
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength,
-      rowIndex * unitLength + unitLength / 2,
+      columnIndex * unitLengthX + unitLengthX,
+      rowIndex * unitLengthY + unitLengthY / 2,
       5,
-      unitLength,
+      unitLengthY,
       {
         label: "wall",
         isStatic: true,
@@ -146,10 +145,10 @@ verticals.forEach((row, rowIndex) => {
 
 //Goal
 const goal = Bodies.rectangle(
-  width - unitLength / 2,
-  height - unitLength / 2,
-  unitLength * 0.7,
-  unitLength * 0.7,
+  width - unitLengthX / 2,
+  height - unitLengthY / 2,
+  unitLengthX * 0.7,
+  unitLengthY * 0.7,
   {
     label: "goal",
     isStatic: true,
@@ -159,7 +158,8 @@ const goal = Bodies.rectangle(
 World.add(world, goal);
 
 //Drawing Ball
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
+const ball = Bodies.circle(unitLengthX / 2, unitLengthY / 2, ballRadius, {
   label: "ball",
 });
 World.add(world, ball);
